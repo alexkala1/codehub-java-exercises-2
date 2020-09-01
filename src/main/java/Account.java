f8b219f88e53da6b8a78d1a00c9b670c4c0ffac8
Alexandros Kalaitzidis <alexkalaitzidis2@gmail.com>
2020-09-01 10:52:50 +0300

    Add skeleton for task 2

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -2,4 +2,45 @@ public class Account {
     private String owner;
     private double balance;
     private int numberOfTransactions;
+
+    public Account() {
+    }
+
+    public Account(String owner, double balance, int numberOfTransactions) {
+        this.owner = owner;
+        this.balance = balance;
+        this.numberOfTransactions = numberOfTransactions;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public int getNumberOfTransactions() {
+        return numberOfTransactions;
+    }
+
+    public void setNumberOfTransactions(int numberOfTransactions) {
+        this.numberOfTransactions = numberOfTransactions;
+    }
+
+    public void withdraw(double amount) {
+
+    }
+
+    public void deposit(double amount) {
+
+    }
 }
