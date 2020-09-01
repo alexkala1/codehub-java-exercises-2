fc990fee6cc4a4bf67266db197daa1e48dd552bf
Alexandros Kalaitzidis <alexkalaitzidis2@gmail.com>
2020-09-02 00:58:26 +0300

    Remove getters and setters

--- src/main/java/kalaitzidis/alexandros/StoreAccount.java
+++ src/main/java/kalaitzidis/alexandros/StoreAccount.java
@@ -5,27 +5,10 @@ public class StoreAccount extends Account {
     private String accountCategory;
 
     public StoreAccount() {
-
     }
 
     public StoreAccount(String storeName, String accountCategory) {
         this.storeName = storeName;
-        this.accountCategory = accountCategory;
-    }
-
-    public String getStoreName() {
-        return storeName;
-    }
-
-    public void setStoreName(String storeName) {
-        this.storeName = storeName;
-    }
-
-    public String getAccountCategory() {
-        return accountCategory;
-    }
-
-    public void setAccountCategory(String accountCategory) {
         this.accountCategory = accountCategory;
     }
 
