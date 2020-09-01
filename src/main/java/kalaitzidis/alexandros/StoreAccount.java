4941c7cad5bed4185d0d02e6514ef365f5b685b3
Alexandros Kalaitzidis <alexkalaitzidis2@gmail.com>
2020-09-02 01:27:30 +0300

    Add changeCategory

--- src/main/java/kalaitzidis/alexandros/StoreAccount.java
+++ src/main/java/kalaitzidis/alexandros/StoreAccount.java
@@ -3,6 +3,7 @@ package kalaitzidis.alexandros;
 public class StoreAccount extends Account {
     private String storeName;
     private String accountCategory;
+    private Categories categories;
 
     public StoreAccount() {
     }
@@ -12,7 +13,21 @@ public class StoreAccount extends Account {
         this.accountCategory = accountCategory;
     }
 
-    public void changeCategory(String Category) {
+    public void changeCategory(StoreAccount store, String Category) {
+        for (Categories category : categories.values()) {
+            if (category.toString().equals(Category)) {
+                store.accountCategory = Category;
+                break;
+            }
+        }
+    }
 
+    @Override
+    public String toString() {
+        return "StoreAccount{" +
+                "storeName='" + storeName + '\'' +
+                ", accountCategory='" + accountCategory + '\'' +
+                ", categories=" + categories +
+                '}';
     }
 }
--- src/main/java/kalaitzidis/alexandros/Ui.java
+++ src/main/java/kalaitzidis/alexandros/Ui.java
@@ -1,7 +1,12 @@
 package kalaitzidis.alexandros;
 
 public class Ui {
+    StoreAccount storeAccount = new StoreAccount();
     public void run() {
-
+        StoreAccount store1 = new StoreAccount("tokalopaidi", "PREMIUM");
+        System.out.println(store1);
+        storeAccount.changeCategory(store1, "BASIC");
+        storeAccount.deposit(500);
+        System.out.println(store1);
     }
 }
