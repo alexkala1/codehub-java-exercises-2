c8d94bab3515d926c4edd24de4e6fa91b803c073
Alexandros Kalaitzidis <alexkalaitzidis2@gmail.com>
2020-09-01 11:03:07 +0300

    Add skeleton for task 5

--- src/main/java/StoreAccount.java
+++ src/main/java/StoreAccount.java
@@ -1,4 +1,43 @@
+import jdk.jfr.Category;
+
 public class StoreAccount extends Account {
     private String storeName;
     private String accountCategory;
+
+    public StoreAccount() {
+
+    }
+
+    public StoreAccount(String storeName, String accountCategory) {
+        this.storeName = storeName;
+        this.accountCategory = accountCategory;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getAccountCategory() {
+        return accountCategory;
+    }
+
+    public void setAccountCategory(String accountCategory) {
+        this.accountCategory = accountCategory;
+    }
+
+    public void withdraw(double amount) {
+
+    }
+
+    public void deposit(double amount) {
+
+    }
+
+    public void changeCategory(String Category) {
+
+    }
 }
